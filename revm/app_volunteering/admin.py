629f895b5eea335fab36ad919a266a382b316b5e
Tudor Amariei <tudor.amariei@code4.ro>
2022-03-05 11:37:46 +0200

    Fix the county filtering check

--- revm/app_volunteering/admin.py
+++ revm/app_volunteering/admin.py
@@ -156,7 +156,7 @@ class AdminVolunteeringRequest(ImportExportModelAdmin):
         if not self.has_view_or_change_permission(request):
             queryset = queryset.none()
 
-        if not request.is_superuser and request.user.is_dsu_user():
+        if not request.user.is_superuser and request.user.is_dsu_user():
             return queryset.filter(county_coverage__contains=request.user.county)
 
         if request.user.is_superuser or request.user.is_dsu_user():
