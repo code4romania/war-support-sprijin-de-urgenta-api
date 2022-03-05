36c02fa604c26380f5610eba845ed0dbd390c815
Tudor Amariei <tudor.amariei@code4.ro>
2022-03-05 14:24:18 +0200

    Add location ACL to transport

--- revm/app_transport_service/admin.py
+++ revm/app_transport_service/admin.py
@@ -155,6 +155,9 @@ class AdminTransportServiceOffer(ImportExportModelAdmin):
         if not self.has_view_or_change_permission(request):
             queryset = queryset.none()
 
+        if not request.user.is_superuser and request.user.is_dsu_manager_user():
+            return queryset.filter(county_coverage__contains=request.user.county)
+
         if request.user.is_superuser or request.user.is_dsu_user():
             return queryset
 
@@ -260,6 +263,9 @@ class AdminTransportServiceRequest(ImportExportModelAdmin):
 
         if not self.has_view_or_change_permission(request):
             queryset = queryset.none()
+
+        if not request.user.is_superuser and request.user.is_dsu_manager_user():
+            return queryset.filter(from_county=request.user.county)
 
         if request.user.is_superuser or request.user.is_dsu_user():
             return queryset
