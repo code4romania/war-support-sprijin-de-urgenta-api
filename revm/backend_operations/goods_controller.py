45d56b8c4d1e30b17bce509ad57d52de3dca20c4
miloradf <miloradf@spotify.com>
2022-03-06 15:24:34 +0100

    skeleton logic separation

---
+++ revm/backend_operations/goods_controller.py
@@ -0,0 +1,28 @@
+'''
+Controller for managing the stock of goods in the system.
+Handles offering goods,
+'''
+class GoodsController:
+    def create_offer(self):
+        pass
+
+    def cancel_offer(self):
+        pass
+
+    def approve_offer(self):
+        pass
+
+    def request(self):
+        pass
+
+    def cancel_request(self):
+        pass
+
+    def approve_request(self):
+        pass
+
+    def complete_request(self):
+        pass
+
+    def close_request(self):
+        pass
