c63cca83ebcbaf41cd96a4baa5a9ba9e0f4bfa06
Tudor Amariei <tudor.amariei@code4.ro>
2022-03-31 09:41:41 +0300

    Fix user fields on creation
    - with proper logic

--- revm/revm_site/urls.py
+++ revm/revm_site/urls.py
@@ -2,9 +2,15 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import (
+    PasswordResetDoneView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 from django.urls import include, path
+from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework import routers
 
@@ -51,40 +57,22 @@ router.register(r"donate/transport_service", CreateTransportServiceOfferViewSet,
 router.register(r"donate/volunteering", CreateVolunteeringOfferViewSet, basename="volunteering_resource")
 
 urlpatterns = i18n_patterns(
+    # Redirect admin to root; must be before the root url path
+    path("admin/", RedirectView.as_view(url="/")),
     # URL patterns which accept a language prefix
-    path(
-        "admin/password_reset/",
-        auth_views.PasswordResetView.as_view(),
-        name="admin_password_reset",
-    ),
-    path(
-        "admin/password_reset/done/",
-        auth_views.PasswordResetDoneView.as_view(),
-        name="password_reset_done",
-    ),
-    path(
-        "admin/reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(),
-        name="password_reset_confirm",
-    ),
-    path(
-        "admin/reset/done/",
-        auth_views.PasswordResetCompleteView.as_view(),
-        name="password_reset_complete",
-    ),
-    path("admin/", admin.site.urls, name="admin"),
-    path("logout", LogoutView.as_view(), name="logout"),
+    path("api/v1/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="swagger-ui"), name="swagger-ui"),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/v1/", include(router.urls)),
     path("auth/", include("dj_rest_auth.urls")),
-    path("registration/", include("dj_rest_auth.registration.urls")),
     path("i18n/", include("django.conf.urls.i18n")),
-    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "api/v1/schema/swagger-ui/",
-        SpectacularSwaggerView.as_view(url_name="swagger-ui"),
-        name="swagger-ui",
-    ),
     path("impersonate/", include("impersonate.urls"), name="impersonate"),
+    path("logout", LogoutView.as_view(), name="logout"),
+    path("password_reset/done/", PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("password_reset/", PasswordResetView.as_view(), name="admin_password_reset"),
+    path("registration/", include("dj_rest_auth.registration.urls")),
+    path("reset/<uidb64>/<token>/", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("reset/done/", PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path("", admin.site.urls, name="admin"),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
@@ -92,5 +80,3 @@ if settings.ENABLE_DEBUG_TOOLBAR:
     import debug_toolbar
 
     urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
-
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
